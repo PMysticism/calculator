196abd89ba7e049e270c73d42ed161e60ed511bd
PMysticism <123906683+PMysticism@users.noreply.github.com>
2026-01-07 22:36:30 -0500

    Adjust column proportions in main.py

--- main.py
+++ main.py
@@ -44,10 +44,10 @@ st.set_page_config(
 )
 
 # Optional: Top navigation using st.page_link
-left, mid, right = st.columns([50, 1, 1])
+left, mid, right = st.columns([9, 1, 1])
 
 with left:
-    col1, col2, col3, col4 = st.columns([1.2, 3, 3, 2.5])
+    col1, col2, col3, col4 = st.columns([1.3, 3, 3, 1.5])
     with col1:
         st.page_link("main.py", label= "Database")
 
